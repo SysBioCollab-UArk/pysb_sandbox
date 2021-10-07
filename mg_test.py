d22770351d9bde1a0d75370c80e0462bda32988b
merang <ryg668@mocs.utc.edu>
2021-10-07 12:49:56 -0400

    update

--- mg_test.py
+++ mg_test.py
@@ -12,25 +12,44 @@ Model()
 # Observables
 # simulation commands
 
+
 Monomer('A', ['b', 'state'], { 'state' : ['U', 'P'] })
 Monomer('B', ['a'])
+Monomer('C', ['X'],{'X' : ['active','inactive']})
+
 
 Parameter('A_init', 100)
 Initial(A(b=None, state='U'), A_init)
+
+
 Parameter('B_init', 100)
 Initial(B(a=None), B_init)
 
+Parameter('C_init',120)     # how many monucules 
+Initial(C(X='inactive'),C_init)
+
+
 Parameter('kf_AB', 1)
 Parameter('kr_AB', 1)
+
+
 Rule('A_binds_B', A(b=None, state='U') + B(a=None) | A(b=1, state='U') % B(a=1), kf_AB, kr_AB)
 
 Parameter('k_A_U_to_P', 1)
 Rule('A_state_change', A(b=1, state='U') % B(a=1) >> A(b=1, state='P') % B(a=1), k_A_U_to_P)
 
+Parameter('k_AP_active_C',0.1)  # parapeter  0.01  , 0.001   C rate gets activate 
+
+Rule('AP_active_C', A(b=1, state='P') % B(a=1) + C(X = 'inactive') >> A(b=1, state='P') % B(a=1) + C(X = 'active'),k_AP_active_C)
+
+
 Observable('A_free', A(b=None))
 Observable('A_phos', A(state='P'))
 # Observable('A_bound', A(b=1) % B(a=1))
 Observable('A_bound', A(b=ANY))
+Observable('C_active',C(X= 'active'))
+Observable('C_inactive',C(X= 'inactive'))
+
 
 tspan = np.linspace(0, 10, 1001)
 sim = ScipyOdeSimulator(model, tspan, verbose=True)
