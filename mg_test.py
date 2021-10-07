ca4bad08980946f66186ef660a0e916b0b17e6c7
merang <ryg668@mocs.utc.edu>
2021-10-07 18:11:18 -0400

    update

--- mg_test.py
+++ mg_test.py
@@ -3,46 +3,64 @@ from pysb.simulator import ScipyOdeSimulator
 import numpy as np
 import matplotlib.pyplot as plt
 
-Model()
 
+
+
+
+########## Creat a model ##########
+###################################
+
+
+
+## model's requirement elements ##
+ 
 # Monomers
 # Parameters
 # Initials
-# Rules
+# Rules ( BioNEtGEn or KAPPA ) 
 # Observables
 # simulation commands
 
 
+
+# Declare a model
+Model()
+
+# Declare the monomers 
 Monomer('A', ['b', 'state'], { 'state' : ['U', 'P'] })
 Monomer('B', ['a'])
 Monomer('C', ['X'],{'X' : ['active','inactive']})
 
 
-Parameter('A_init', 100)
+
+
+# Declare parameters              
+Parameter('kf_AB', 1)
+Parameter('kr_AB', 1)
+Parameter('A_init', 100)                # nunmber of molecules (Concentrations )
+Parameter('B_init', 100)                # nunmber of molecules (Concentrations )
+Parameter('C_init',150)                 # number of molecules  (Concentrations ) 
+Parameter('k_A_U_to_P', 1)
+Parameter('k_AP_active_C',0.1)          # C gets activate (e.g, 0.1 , 0.01 ,0.001 )
+
+
+
+
+# Declare inintial conditions 
 Initial(A(b=None, state='U'), A_init)
-
-
-Parameter('B_init', 100)
 Initial(B(a=None), B_init)
-
-Parameter('C_init',120)     # how many monucules 
 Initial(C(X='inactive'),C_init)
 
 
-Parameter('kf_AB', 1)
-Parameter('kr_AB', 1)
 
-
+# Declare binding rules  
 Rule('A_binds_B', A(b=None, state='U') + B(a=None) | A(b=1, state='U') % B(a=1), kf_AB, kr_AB)
-
-Parameter('k_A_U_to_P', 1)
 Rule('A_state_change', A(b=1, state='U') % B(a=1) >> A(b=1, state='P') % B(a=1), k_A_U_to_P)
-
-Parameter('k_AP_active_C',0.1)  # parapeter  0.01  , 0.001   C rate gets activate 
-
 Rule('AP_active_C', A(b=1, state='P') % B(a=1) + C(X = 'inactive') >> A(b=1, state='P') % B(a=1) + C(X = 'active'),k_AP_active_C)
 
 
+
+# observe the complex 
 Observable('A_free', A(b=None))
 Observable('A_phos', A(state='P'))
 # Observable('A_bound', A(b=1) % B(a=1))
@@ -51,17 +69,35 @@ Observable('C_active',C(X= 'active'))
 Observable('C_inactive',C(X= 'inactive'))
 
 
-tspan = np.linspace(0, 10, 1001)
+# Declare the simulate time 
+tStart = 0                          # start time 
+tEnd = 10                           # End time 
+tPoints = 1001                      # number of time-points 
+
+#from pylab import linspace
+#tspan = linspace(tStart,tEnd, tDivid)
+
+tspan = np.linspace(tStart,tEnd, tPoints)
+print ("Simulating.........")
+
+
+
+# Simulate the model 
 sim = ScipyOdeSimulator(model, tspan, verbose=True)
 result = sim.run()
-
 obs = result.observables
+
+
+
+
+# plot the trajectories 
 
 for o in model.observables:
     plt.plot(tspan, obs[o.name], lw=2, label=o.name)
-plt.xlabel('time')
-plt.ylabel('concentration')
-plt.legend(loc=0)
+    
+plt.xlabel('time (secounds)')
+plt.ylabel('Concentration')
+plt.legend(loc=1)              # legend location on the plot (1,2,3,4)
 
 print(model)
 print(model.monomers)
@@ -71,4 +107,6 @@ print(model.observables)
 for ic in model.initial_conditions:
     print(ic)
 
+
 plt.show()
+
